f6200d0b0212dd3ede79d53fc182fc9c8a38330e
moseskamira <moses.african@gmail.com>
2020-08-04 15:14:17 +0300

    IDE Settup

---
+++ app/src/main/java/com/example/gpslocator/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.gpslocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText locationEt;
+    Button locationBtn;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    private static final int REQUEST_CODE = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        locationEt = findViewById(R.id.location_et);
+        locationBtn = findViewById(R.id.location_btn);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        instantiateLocationListner();
+        checkBuildVersion();
+
+    }
+
+    private void instantiateLocationListner() {
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                locationEt.append("\n" + location.getLatitude() + "\n" + location.getLongitude());
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+
+            }
+        };
+    }
+
+    private void checkBuildVersion() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager
+                    .PERMISSION_GRANTED && checkSelfPermission(Manifest.permission
+                    .ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission
+                        .ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
+                }, REQUEST_CODE);
+
+                return;
+            } else {
+                fetchCurrentLocation();
+
+            }
+        }else {
+            fetchCurrentLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantedResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantedResults.length > 0 && grantedResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchCurrentLocation();
+                    return;
+                }
+        }
+
+    }
+
+
+    public void fetchCurrentLocation() {
+
+        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+
+
+    }
+}
